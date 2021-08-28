9fbab58cf76883eb7f0381a6a3fdb2a98ebe17f1
Paul Krisadaolarn <pauly.k@gmail.com>
2021-08-28 14:41:14 +0700

    Always copy appsetting.json to output

--- src/BlobService.cs
+++ src/BlobService.cs
@@ -1,10 +1,10 @@
 using Azure.Storage.Blobs;
 
 namespace AzBlobApiPOC {
-    public class BlobHeler {
+    public class BlobService {
 
         readonly BlobServiceClient blobServiceClient;
-        public BlobHeler(string connection) {
+        public BlobService(string connection) {
             blobServiceClient = new BlobServiceClient(connection);
         } 
 
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
-namespace AzBlobApiPOC
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace AzBlobApiPOC {
+
+
+    class Program {
+        static void Main(string[] args) {
+            var config = GetConfig();
+            
+            BlobService blobService = new(config["AZURE_STORGAE_CONNECTION"]);
+
+        }
+
+        static IConfiguration GetConfig() {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables();
+            return builder.Build();
         }
     }
 }
